2535dcc4dce1d00a944a3e13fd49e5d668a14649
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-05 16:16:42 +0100

    Define getoff2 path

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -2,6 +2,7 @@
 
 #. ../setup.sh
 export PYTHONPATH=$PWD/src/python
+export PATH=$PATH:$PWD/bin
 
 #python -m unittest tests.test_cltools tests.test_daophot  tests.test_astrometry
 python -m unittest tests.test_cltools
