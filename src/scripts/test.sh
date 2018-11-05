df556823f1c85a786783dc2295d4f3485f4c9622
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-05 16:56:11 +0100

    Add path for allstarts and daophot

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -2,7 +2,7 @@
 
 #. ../setup.sh
 export PYTHONPATH=$PWD/src/python
-export PATH=$PATH:$PWD/bin
+export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/
 
 python -m unittest tests.test_cltools tests.test_daophot  tests.test_astrometry
 #python -m unittest tests.test_cltools
