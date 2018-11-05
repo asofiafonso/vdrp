56b72a4a5d9588662d6f4e021280484180093785
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-05 16:17:59 +0100

    Allow run all tests

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -4,5 +4,5 @@
 export PYTHONPATH=$PWD/src/python
 export PATH=$PATH:$PWD/bin
 
-#python -m unittest tests.test_cltools tests.test_daophot  tests.test_astrometry
-python -m unittest tests.test_cltools
+python -m unittest tests.test_cltools tests.test_daophot  tests.test_astrometry
+#python -m unittest tests.test_cltools
