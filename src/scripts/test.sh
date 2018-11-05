f3edc9e2c338ab38a14557856b628aa969a6e274
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-05 17:03:54 +0100

    Correct path for daomaster

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -2,7 +2,7 @@
 
 #. ../setup.sh
 export PYTHONPATH=$PWD/src/python
-export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/daomaster
+export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/
 
 python -m unittest tests.test_cltools tests.test_daophot  tests.test_astrometry
 #python -m unittest tests.test_cltools
