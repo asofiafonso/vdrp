4e74831381b27381c29f1d007c8fc32b5efc0cfc
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-06 16:35:25 +0100

    Really test tests.test_daophot

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -5,6 +5,6 @@ export PYTHONPATH=$PWD/src/python
 export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/
 
 python -m unittest tests.test_cltools tests.test_daophot tests.test_astrometry
-python -m unittest tests.test_cltools
-##python -m unittest tests.test_daophot
+##python -m unittest tests.test_cltools #warnings that my be a problem
+python -m unittest tests.test_daophot
 ##python -m unittest tests.test_astrometry
