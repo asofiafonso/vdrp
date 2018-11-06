d8f016b972ea866ea5e654bc788feb5c10306355
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-06 17:31:44 +0100

    Found daomaster

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -2,9 +2,9 @@
 
 #. ../setup.sh
 export PYTHONPATH=$PWD/src/python
-export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/
+export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/daomaster
 
 ##python -m unittest tests.test_cltools tests.test_daophot tests.test_astrometry
 ##python -m unittest tests.test_cltools #3 tests and OK
 ##python -m unittest tests.test_daophot #6 tests and OK
-python -m unittest tests.test_astrometry
+python -m unittest tests.test_astrometry #Getting warnings, backetrace
