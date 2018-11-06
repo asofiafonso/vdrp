d696d98a3a4a974d63223fb910b0815fca877eaf
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-06 16:39:20 +0100

    Really tests.test_astrometry

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -6,5 +6,5 @@ export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhar
 
 ##python -m unittest tests.test_cltools tests.test_daophot tests.test_astrometry
 ##python -m unittest tests.test_cltools #3 tests and OK
-python -m unittest tests.test_daophot
-##python -m unittest tests.test_astrometry
+##python -m unittest tests.test_daophot #6 tests and OK
+python -m unittest tests.test_astrometry
