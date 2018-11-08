3a31ce507f0dae693e949d2d9b792c937839fdfd
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-08 17:19:41 +0100

    test10

--- src/python/tests/test_astrometry.py
+++ src/python/tests/test_astrometry.py
@@ -16,9 +16,9 @@ from vdrp.astrometry import main
 import glob
 import path
 
-# class Test_Get_Exposures_files(TestBase):
-#     ff = ["20180611T054545_015.fits", "20180611T055249_015.fits", "20180611T060006_015.fits"]
-#
+ class Test_Get_Exposures_files(TestBase):
+     ff = ["20180611T054545_015.fits", "20180611T055249_015.fits", "20180611T060006_015.fits"]
+
 #     def test_get_exposures_files(self):
 #         with path.Path(self.test_dir):
 #             exposures_files = astrometry.get_exposures_files(self.test_dir)
