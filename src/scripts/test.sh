70f79ab1dd5d99663b68f698f0c9843733802a8b
Ana Paulio-Afonso <asofiafonso@gmail.com>
2018-11-08 17:32:14 +0100

    all test

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -4,10 +4,10 @@
 export PYTHONPATH=$PWD/src/python
 export PATH=$PATH:$PWD/bin:/home/00115/gebhardt/lib/daoprogs/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/:/home/00115/gebhardt/lib/daoprogs/moreprogs2/daomaster
 
-##python -m unittest tests.test_cltools tests.test_daophot tests.test_astrometry
+python -m unittest tests.test_cltools tests.test_daophot tests.test_astrometry
 #python -m unittest tests.test_cltools #3 tests and OK
 #python -m unittest tests.test_daophot #6 tests and OK
 #python -m unittest tests.test_astrometry #1 test and OK
 #python -m unittest tests.test_cltools tests.test_daophot #9 tests and ok
 #python -m unittest tests.test_cltools tests.test_astrometry #4 tests and OK
-python -m unittest tests.test_daophot tests.test_astrometry #
+#python -m unittest tests.test_daophot tests.test_astrometry #7 tests and OK
